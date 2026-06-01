da4955ac1d8776cf1871ca0ab00d60e58f7bac58
Devendra Vyas <vyas.dev.ms@gmail.com>
2026-06-01 12:43:01 +0200

    Add logs for debugging

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'sendToWebhook') {
+    console.log('Sending to:', request.webhookUrl);
+    console.log('Data:', JSON.stringify(request.data));
     fetch(request.webhookUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(request.data)
     })
-    .then(() => sendResponse({ success: true }))
-    .catch(err => sendResponse({ success: false, error: err.message }));
+    .then(res => {
+      console.log('Response status:', res.status);
+      sendResponse({ success: true });
+    })
+    .catch(err => {
+      console.log('Fetch error:', err.message);
+      sendResponse({ success: false, error: err.message });
+    });
     return true;
   }
 });
